f152f4cd75222fc973bd996be776525125992505
Jess Gardner <inanna1130@gmail.com>
2017-09-06 14:52:01 -0400

    created routes and methods in controller

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -1,2 +1,50 @@
 class CreaturesController < ApplicationController
+
+    def index
+        @creatures = Creature.all
+    end
+
+    def new
+        @creature = Creature.new
+    end
+
+    def show
+        @creature = Creature.find(params[:id])        
+    end
+
+    def create
+        @creature = Creature.create(creature_params)
+
+        if @creature.save
+            redirect_to creature_path(@creature)
+        end
+    end
+
+    def edit
+        @creature = Creature.find(params[:id])
+    end
+
+    def update
+        @creature = Creature.find(params[:id])
+        @creature.update   
+        
+        if @creature.save
+            redirect_to creature_path(@creature)
+        end
+    end
+
+    def delete
+        @creature = Creature.find(params[:id])
+        @creature.destroy
+        
+        redirect_to creature_path
+        
+    end
+
+    private
+    def creature_params
+        params.require(:creature).permit(:name, :description)
+    end
+
+
 end
